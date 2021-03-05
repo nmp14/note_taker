2e503843080c1b963b9f5cc684f6722b01a651b5
Nathan Peek <nmp14fsu@gmail.com>
2021-03-05 02:14:54 -0500

    Add a few paths for html and static files as well as a get requewst for notes

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ const path = require("path");
 const app = express();
 const PORT = 8080;
 
+let notes = [];
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Paths
+app.use("/assets", express.static('assets'));
+// HTML paths
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+// API paths
+app.get("/api/notes", (req, res) => res.json(notes));
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
